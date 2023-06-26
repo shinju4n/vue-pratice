253a7e028e43b8a77eed35e82628d2e7d481dbf8
Tim <ju4nshin@gmail.com>
2023-06-26 16:06:52 +0900

    add : Post Detail 동적 라우팅 구현

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -3,7 +3,7 @@
     <h2>게시글 상세: {{ $route.params.id }}</h2>
     <p>params : {{ $route.params }}</p>
     <p>query : {{ $route.query }}</p>
-    <hr class="my-4" />
+    <hr class="my-4"/>
   </div>
 </template>
 
--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -2,9 +2,41 @@
   <div>
     <h2>게시글 목록</h2>
     <hr class="my-4" />
+    <div class="row g-3">
+      <div v-for="post in posts" :key="post.id" class="col-4">
+        <PostItem :title="post.title" :content="post.content" :createdAt="post.createdAt" @click="goPage(post.id)" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import PostItem from '@/components/posts/PostItem.vue';
+import {getPosts} from '@/api/posts'
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const posts = ref([]);
+const router = useRouter()
+
+const fetchPosts = async () => {
+  posts.value = await getPosts();
+}; 
+
+const goPage = (id) => {
+  router.push({
+    name: 'PostDetail',
+    params: {
+      id: id
+    },
+    query: {
+      search : 'Hello World'
+    }
+  });
+};
+
+fetchPosts();
+
+</script>
 
 <style lang="scss" scoped></style>
