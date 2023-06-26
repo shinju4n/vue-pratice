c896c3243743e4eeaf16b3e4b8ec3f97b76b5c20
Tim <ju4nshin@gmail.com>
2023-06-26 14:47:12 +0900

    add : router/params,query 추출 예시

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2>게시글 상세: {{ $route.params.id }}</h2>
+    <p>params : {{ $route.params }}</p>
+    <p>query : {{ $route.query }}</p>
     <hr class="my-4" />
   </div>
 </template>
